0343187dd467f4d3e2bee9f14a2d24a99af5d4f1
HUYNH CHI NGHIA <22.huynhchinghia.a2@gmail.com>
2026-01-07 16:52:22 +0700

    foundation

---
+++ nested loops.js
@@ -0,0 +1,11 @@
+for(var i=0;i<2;i++){
+    for(var j=0;j<=10;j++){
+        console.log(i+' times '+j+' equals '+i*j)
+    }
+}
+
+for(var a=100;a>0;a=a-10){
+    for(b=10;b>4;b=b-5){
+        console.log(a+' divided by '+b +' equal '+ a/b)
+    }
+}
